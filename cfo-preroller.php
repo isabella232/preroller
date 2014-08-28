7248b9e3ba02b1eb1f947f82ed768633a88c4481
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-08-28 17:23:39 -0400

    Doing basic class setup

--- cfo-preroller.php
+++ cfo-preroller.php
@@ -1,5 +1,40 @@
 <?php
 require_once('cfo-plugin.php');
 class CFO_Preroller extends CFO_Plugin {
-  
+
+	/**
+	 * Set up variables for the class.
+	 */
+	protected function setup_globals() {
+
+		$this->file       = __FILE__;
+		$this->basename   = apply_filters( 'CFO_Preroller_plugin_basename', plugin_basename( $this->file ) );
+		$this->plugin_dir = apply_filters( 'CFO_Preroller_plugin_dir_path', plugin_dir_path( $this->file ) );
+		$this->plugin_url = apply_filters( 'CFO_Preroller_plugin_dir_url',  plugin_dir_url( $this->file ) );
+
+	}
+
+	/**
+	 * Load required or conditional includes
+	 */
+	protected function includes() {
+
+	}
+
+  public function setup_actions(){
+    add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+  }
+
+	/**
+	 * Enqueue Preroller Scripts
+	 *
+	 * @return null
+	 */
+	public function scripts() {
+		wp_enqueue_script( 'videojs-473', $this->plugin_url . '/library/videojs/video.js', array('jquery'), '4.7.3' );
+    wp_enqueue_script( 'videojs-youtube', $this->plugin_url . '/library/videojs-youtube/src/youtube.js', array('jquery', 'videojs-473') );
+    wp_enqueue_script( 'videojs-vast-plugin', $this->plugin_url . '/library/videojs_vast_ad_serving_plugin/js/vast.plugin.js', array('jquery', 'videojs-473', 'videojs-youtube') );
+	}
+
+
 }
